a59d3291143b84473f0949c1aa9b1f47ba829cf3
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:18:04 +0700

    Buat tata letak awal dan latar belakang

---
+++ app/src/main/java/com/example/prak5/view/LayarSelamatDatang.kt
@@ -0,0 +1,29 @@
+package com.example.prak5.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn (ExperimentalMaterial3Api::class)
+@Composable
+fun Home (
+    OnMasukBtnClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF949494))
+            .padding (horizontal = 24.dp, vertical = 32.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+    }
+}
