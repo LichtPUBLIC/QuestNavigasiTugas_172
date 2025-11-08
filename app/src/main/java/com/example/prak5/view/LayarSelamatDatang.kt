dea6c010d47bc067ad742ec0c59399f213c03a86
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:23:50 +0700

    Tambahkan info nama dan NIM

--- app/src/main/java/com/example/prak5/view/LayarSelamatDatang.kt
+++ app/src/main/java/com/example/prak5/view/LayarSelamatDatang.kt
@@ -1,13 +1,24 @@
 package com.example.prak5.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.prak5.R
 
@@ -25,6 +36,12 @@ fun Home (
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
+            text = stringResource(id = R.string.judul_welcome),
+            style = MaterialTheme.typography.titleLarge.copy (
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp
+            ),
+            textAlign = TextAlign.Center
         )
 
         Image(
@@ -34,5 +51,22 @@ fun Home (
                 .size(300.dp)
                 .padding(vertical = 16.dp)
         )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = stringResource(id = R.string.nama),
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = stringResource(id = R.string.nim),
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
