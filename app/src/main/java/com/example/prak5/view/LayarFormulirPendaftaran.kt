992116afdb2cea34f2fd89b40d77b1c30baa1d90
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:31:14 +0700

    Tambahkan field input Nama dan Alamat

--- app/src/main/java/com/example/prak5/view/LayarFormulirPendaftaran.kt
+++ app/src/main/java/com/example/prak5/view/LayarFormulirPendaftaran.kt
@@ -1,6 +1,7 @@
 package com.example.prak5.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -65,9 +67,25 @@ fun FormInput(
             Column(
                 modifier = Modifier
                     .padding(16.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
 
+                Text(text = stringResource(id = R.string.nama_lengkap))
+                OutlinedTextField(
+                    value = textNama,
+                    onValueChange = { textNama = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = { Text("Masukkan nama lengkap") }
+                )
+
+                Text(text = stringResource(id = R.string.alamat))
+                OutlinedTextField(
+                    value = textAlamat,
+                    onValueChange = { textAlamat = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = { Text("Masukkan alamat lengkap") }
+                )
             }
         }
     }
