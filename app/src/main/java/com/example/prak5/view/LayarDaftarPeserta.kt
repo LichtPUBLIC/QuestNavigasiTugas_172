72c414ec4a2d22fd274e15b34ee0e6212124a7c9
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:27:12 +0700

    Tambahkan judul 'List Daftar Peserta'

--- app/src/main/java/com/example/prak5/view/LayarDaftarPeserta.kt
+++ app/src/main/java/com/example/prak5/view/LayarDaftarPeserta.kt
@@ -3,15 +3,21 @@ package com.example.prak5.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.prak5.R
 
 @OptIn (ExperimentalMaterial3Api::class)
@@ -32,7 +38,14 @@ fun ListPeserta(
         Column(
             modifier = Modifier.fillMaxWidth()
         ) {
-
+            Text (
+                text = stringResource(id = R.string.judul_list),
+                style = MaterialTheme.typography.titleLarge.copy(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(top = 30.dp, bottom = 30.dp),
+            )
         }
         Row(
             modifier = Modifier.fillMaxWidth()
