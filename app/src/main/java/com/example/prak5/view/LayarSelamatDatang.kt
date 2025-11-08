cdd2ceccaade6fb269afbc63caa3808651af84ec
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:18:37 +0700

    Tambahkan teks judul 'Selamat Datang'

--- app/src/main/java/com/example/prak5/view/LayarSelamatDatang.kt
+++ app/src/main/java/com/example/prak5/view/LayarSelamatDatang.kt
@@ -1,16 +1,12 @@
 package com.example.prak5.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.background
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.example.prak5.R
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -25,5 +21,13 @@ fun Home (
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = stringResource(id = R.string.judul_welcome),
+            style = MaterialTheme.typography.titleLarge.copy (
+                fontWeight = FontWeight.Bold,
+                fontSize = 35.sp
+            ),
+            textAlign = TextAlign.Center
+        )
     }
 }
