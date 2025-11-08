47a3a437e5115be146a865c5cadeac51a172d03d
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-08 20:31:46 +0700

    Tambahkan field RadioButton Jenis Kelamin dan Status

--- app/src/main/java/com/example/prak5/view/LayarFormulirPendaftaran.kt
+++ app/src/main/java/com/example/prak5/view/LayarFormulirPendaftaran.kt
@@ -3,21 +3,25 @@ package com.example.prak5.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -78,6 +82,50 @@ fun FormInput(
                     modifier = Modifier.fillMaxWidth(),
                     placeholder = { Text("Masukkan nama lengkap") }
                 )
+
+                Text(text = stringResource(id = R.string.jenis_kelamin))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    genderList.forEach { item ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.selectable(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            )
+                        ) {
+                            RadioButton(
+                                selected = textJK == item,
+                                onClick = { textJK = item }
+                            )
+                            Text(text = item)
+                        }
+                    }
+                }
+
+                Text(text = stringResource(id = R.string.status_perkawinan))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    statusList.forEach { item ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.selectable(
+                                selected = textStatus == item,
+                                onClick = { textStatus = item }
+                            )
+                        ) {
+                            RadioButton(
+                                selected = textStatus == item,
+                                onClick = { textStatus = item }
+                            )
+                            Text(text = item)
+                        }
+                    }
+                }
 
                 Text(text = stringResource(id = R.string.alamat))
                 OutlinedTextField(
